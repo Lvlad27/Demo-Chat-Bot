c8f5d85d6715a0b1bdce76685d8da595cf00c393
Vlad Leontescu <lvlad27@gmail.com>
2023-04-30 22:10:26 +0300

    Create a custom header

--- app.js
+++ app.js
@@ -2,9 +2,17 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req,res) => {
-	res.send('Hello Vlad!');
+app.get('/', (req, res) => {
+  res.send('Hello Vlad!');
+});
+
+app.get('/exercise', (req, res) => {
+  res
+    .status(418)
+    .set({
+      'X-Vlad-Header': 'Test exercise',
+    })
+    .send('This is an exercise');
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
